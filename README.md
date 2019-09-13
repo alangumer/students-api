aaf368946ed6ffa343b7c2c5b74d2c2c6827f927
Henry Orozco <alangumer@gmail.com>
2019-09-13 05:58:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# students-api
+This project was bootstrapped with [Loopback](https://loopback.io)
+
+For the Datasource, we are using the default In-memory db connector, data found at data/db.json.
+## Build and run it
+```shell
+npm install
+npm start
+```
---
+++ src/models/student.model.ts
@@ -0,0 +1,34 @@
+import { Entity, model, property } from '@loopback/repository';
+
+@model({ settings: {} })
+export class Student extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  birthdate: string;
+
+
+  constructor(data?: Partial<Student>) {
+    super(data);
+  }
+}
+
+export interface StudentRelations {
+  // describe navigational properties here
+}
+
+export type StudentWithRelations = Student & StudentRelations;
